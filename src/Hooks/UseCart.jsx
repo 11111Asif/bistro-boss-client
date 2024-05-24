9994a1c5005332b6fd2662765abbb6aa358d7df1
11111Asif <asif040502@gmail.com>
2024-05-24 15:05:11 +0600

    added

--- src/Hooks/UseCart.jsx
+++ src/Hooks/UseCart.jsx
@@ -1,13 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
 import UseAxiosSecure from "./UseAxiosSecure";
+import UseAuth from "./UseAuth";
 
 
 const UseCart = () => {
+    const {user} = UseAuth()
     const axiosSecure = UseAxiosSecure()
+    
    const {data: cart=[]} = useQuery({
-    queryKey: ['cart'],
+    queryKey: ['cart', user?.email],
     queryFn: async () => {
-        const res = await axiosSecure.get('/carts')
+        const res = await axiosSecure.get(`/carts?email=${user.email}`)
         return res.data;
     }
    })
